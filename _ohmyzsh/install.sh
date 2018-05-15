ced113761e11274d8c729b886532aa319dd381a9
Calvin Mwesigwa <cmwesigw@gmail.com>
2018-05-14 22:51:46 -0500

    Add dropbox to Brewfile. Fix some install conditions.

--- _ohmyzsh/install.sh
+++ _ohmyzsh/install.sh
@@ -5,7 +5,7 @@
 # This installs OhMyZsh
 
 # Check for OhMyZsh
-if [ ! -f ~/.oh-my-zsh ];
+if [ ! -d ~/.oh-my-zsh ];
 then
   echo "  Installing OhMyZsh for you."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
--- janus/install.sh
+++ janus/install.sh
@@ -5,7 +5,7 @@
 # This installs Janus for Vim
 
 # Check for Janus
-if [ ! -f ~/.vimrc.after ];
+if [ ! -d ~/.vim/janus ];
 then
   echo "  Installing Janus for you."
   curl -Lo- https://bit.ly/janus-bootstrap | bash
