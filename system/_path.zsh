fbc56c6dd8bb5cae0c9b2e013ebc56ab8a61d805
mwesigwa <cmwesigw@gmail.com>
2019-09-26 13:01:48 -0500

    Add Python bin to path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH_CONFIG/bin:$PATH"
+export PATH="./Library/Python/3.7/bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH_CONFIG/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
